037351e349e7dcb8da4e7d536079d057ee36e8c3
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-02-15 23:49:18 +1300

    change navbar theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
 
   return (
     // Navbar container with Bootstrap classes
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      {/* Username, Name, and Icon */}
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+      {/* Username Icon */}
       <div className="d-flex align-items-center order-0">
         <span className="navbar-text mr-3">Putnam</span>
         <i className="fas fa-user-circle"></i>
@@ -24,9 +24,9 @@ const Navbar = () => {
       </button>
 
       {/* Collapsible content */}
-      <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'} ml-auto`} id="navbarSupportedContent1">
+      <div className={` collapse navbar-collapse ${isCollapsed ? '' : 'show'} `} id="navbarSupportedContent1">
         {/* Navbar links */}
-        <ul className="navbar-nav">
+        <ul className="navbar-nav ml-auto ">
           {/* Home link */}
           <li className="nav-item active">
             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
