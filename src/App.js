1fc77afb348b3634943a68a6d915771882ed6e15
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-02-15 21:54:03 +1300

    add bootstrap main container and navbar

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
+    <div className="container-fluid bg-light text-center">
+    Container
+    <div className='navbar bg-primary text-white'>
+      Navbar
+    </div>
     </div>
   );
 }
